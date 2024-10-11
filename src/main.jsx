e1f9b5be35bdd4f865cbd732a6ab956be03e73a8
enricofs <enricoferreiradossantos@gmail.com>
2024-10-11 01:07:31 -0300

    :sparkles: feat(page): add chats page

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,18 @@ import Error404 from "./routes/404";
 import Connections from "./routes/students/connections";
 import Jobs from "./routes/students/jobs";
 import HomePage from "./routes/students/home";
+import Chats from "./routes/students/chats";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    errorElement: <Error404/>,
+    element: <App />,
+    errorElement: <Error404 />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/connections", element: <Connections /> },
       { path: "/jobs", element: <Jobs /> },
+      { path: "/chats", element: <Chats /> },
     ],
   },
 ]);
