a5851d71603bdacc9a6458c85b6e6dfa7b297ce1
enricofs <enricoferreiradossantos@gmail.com>
2024-11-05 00:28:58 -0300

    :recycle: refactor(login): recomponentize login page structure

--- src/routes/loginAndRegister/login/index.jsx
+++ src/routes/loginAndRegister/login/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import styles from "./style.module.css";
-import LoginForm from "../components/loginForm";
+import Container from "../components/container";
+import GroupBox from "../components/groupBox";
+import Logo from "../components/logo";
+import Title from "../components/title";
+import SubmitButton from "../components/submitButton";
 import LoadingOverlay from "../components/loadingOverlay";
+import SideBanner from "../components/sideBanner";
+import InputField from "../components/inputField";
+import PasswordField from "../components/passwordField";
+import styles from "./style.module.css";
 
 export default function Login() {
-  const [loading, setLoading] = React.useState(false); // Estado de carregamento
+  const [isLoading, setLoading] = React.useState(false); // Estado de carregamento
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,83 +20,38 @@ export default function Login() {
   };
 
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.loginBox}>
-          <div className={styles.logo}>
-            <img src="/laborus-logotipo-42x42.png" alt="Logotipo Laborus" />
-          </div>
-          <LoginForm onSubmit={handleSubmit} />
-        </div>
-        <div className={styles.sideBanner}></div>
-      </div>
-      {loading && <LoadingOverlay />} {/* Exibe o overlay de carregamento */}
-    </>
+    <Container>
+      <GroupBox>
+        <Logo />
+
+        <form onSubmit={handleSubmit}>
+          <Title
+            title="Bem-vindo(a) de volta!"
+            details="É bom vê-lo(a) novamente! Digite suas informações."
+          />
+
+          <InputField
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            errorMessage="Os campos não podem estar vazios."
+          />
+
+          <PasswordField />
+
+          <a href="#" className={styles.forgotPassword}>
+            Esqueceu a senha?
+          </a>
+
+          <SubmitButton type="submit" />
+
+          <p className={styles.registerLink}>
+            Não possui conta ainda? <a href="#">Cadastre-se.</a>
+          </p>
+        </form>
+      </GroupBox>
+      <SideBanner />
+      {isLoading && <LoadingOverlay />}
+    </Container>
   );
 }
-
-// import React, { useState } from "react";
-// import styles from "./Login.module.css";
-
-// import { FaEye, FaEyeSlash } from "react-icons/fa"; // Ícones Font Awesome
-
-// export default function Login() {
-//   const [showPassword, setShowPassword] = useState(false); // Estado para mostrar/ocultar senha
-//   const [loading, setLoading] = useState(false); // Estado de carregamento
-
-//   const togglePasswordVisibility = () => {
-//     setShowPassword((prev) => !prev);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     setLoading(true); // Simula o carregamento
-//     setTimeout(() => setLoading(false), 2000); // Exemplo de espera simulada
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.loginBox}>
-//         <div className={styles.logo}>
-//           <img src="/laborus-logotipo-42x42.png" alt="Logotipo Laborus" />
-//         </div>
-//         <h1 className={styles.title}>Bem-vindo(a) de volta!</h1>
-//         <p className={styles.titleDetails}>
-//           É bom vê-lo(a) novamente! Digite suas informações.
-//         </p>
-//         <form id="login-form" onSubmit={handleSubmit}>
-//           <div className={`${styles.inputBox} ${styles.emailBox}`}>
-//             <input type="email" name="email" placeholder="E-mail" required />
-//             <span className={`${styles.error} ${styles.emailError}`}>
-//               Os campos não podem estar vazios.
-//             </span>
-//           </div>
-//           <div className={styles.inputBox}>
-//             <input
-//               type={showPassword ? "text" : "password"}
-//               name="password"
-//               placeholder="Senha"
-//               required
-//             />
-//             <i
-//               onClick={togglePasswordVisibility}
-//               className={styles.togglePassword}
-//             >
-//               {showPassword ? <FaEyeSlash /> : <FaEye />}
-//             </i>
-//           </div>
-//           <a href="#" className={styles.forgotPassword}>
-//             Esqueceu a senha?
-//           </a>
-//           <button type="submit" className={styles.loginButton}>
-//             Entrar
-//           </button>
-//         </form>
-//         <p className={styles.registerLink}>
-//           Não possui conta ainda? <a href="#">Cadastre-se.</a>
-//         </p>
-//       </div>
-//       <div className={styles.sideBanner}></div>
-//     </div>
-//   );
-// }
